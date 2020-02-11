3cbfd94232b7ec27cabcff07a1c81b40e9467b41
mesarikaya <mesarikaya@gmail.com>
2020-02-11 17:26:37 +0100

    Task: Initial project add. Closes #1

---
+++ src/test/java/com/mes/beermicroservice/BeerMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mes.beermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
