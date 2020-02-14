fe645eaa6bd14b68d46be965e49648134f473179
mesarikaya <mesarikaya@gmail.com>
2020-02-14 13:59:43 +0100

    Add JSON mapping for Paged Object BeerDto. Closes #12

--- target/generated-sources/annotations/com/mes/beermicroservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/mes/beermicroservice/web/mappers/BeerMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-02-14T01:00:03+0100",
+    date = "2020-02-14T13:57:45+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.2 (AdoptOpenJDK)"
 )
 @Component
